f9bee9d1eaee9debc453defd49cf6ba5ccdc38c7
Takayuki Matsumura <op.66.allegro.agitato@gmail.com>
2022-02-03 23:36:45 +0900

    ビルドして確認するところまで

---
+++ Assets/Scripts/TestMonoBehaviour.cs
@@ -0,0 +1,32 @@
+using Sentry;
+using UnityEngine;
+
+public class TestMonoBehaviour : MonoBehaviour
+{
+    private GameObject testObject = null;
+
+    void Start()
+    {
+        //Debug.Log("Captured Log");              // Breadcrumb
+        //Debug.LogWarning("Captured Warning");   // Breadcrumb
+        //Debug.LogError("Captured Error");       // Captured Error
+        //Debug.LogError("New Error");
+        // This will throw a Null Reference Exception
+        //testObject.GetComponent<Transform>();   // Captured error
+    }
+
+    public void OnClickWarningButton()
+    {
+        Debug.LogWarning("Warning");
+    }
+
+    public void OnClickErrorButton()
+    {
+        Debug.LogError("Error");
+    }
+
+    public void OnClickExceptionButton()
+    {
+        throw new System.Exception("Exception");
+    }
+}
